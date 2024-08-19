fe30801bee2469f3c6c8d8c18d112195b7d62b2c
samh7 <samh7@gmail.com>
2024-08-19 03:46:34 +0300

    UI changes and now checks whether phone number has strings

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const openButton = document.getElementById("open-button");
 const phoneNumber = document.getElementById("number-input");
+const regex = /^[0-9]+$/;
+
 openButton.addEventListener("click", function () {
   if (`${phoneNumber.value}`.length === 10) {
-    window.location.href = `https://wa.me/+254${phoneNumber.value}`;
+    if (regex.test(`${phoneNumber.value}`)) {
+      window.location.href = `https://wa.me/+254${phoneNumber.value}`;
+    }
   }
 });
